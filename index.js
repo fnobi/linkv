7063529ed2f8433028169121c2b884e07dbe3d62
fnobi <shin@fnobi.com>
2012-10-20 12:33:53 +0900

    first commit.

---
+++ index.js
@@ -0,0 +1,30 @@
+var request   = require('request');
+var cheerio   = {
+       select : require('cheerio-select'),
+       parse  : require('cheerio')
+};
+
+var linkv = function (url, callback) {
+	request.get(url, function (err, res, body) {
+		if (err) {
+			return callback(err);
+		}
+
+		var title = getTitle(body);
+
+		var contents = [
+			'[' + title + ']',
+			' - ' + url
+		].join('\n');
+
+		return callback(null, contents);
+	});
+};
+
+var getTitle = function (html) {
+	var dom = cheerio.parse(html);
+	var filteredDom = cheerio.select('title', dom);
+	return filteredDom[0].children[0].data;
+};
+
+module.exports = linkv;
