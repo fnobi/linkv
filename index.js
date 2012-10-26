0cd6b38b7e0b06bfb0e0c55be0ff93af418c4484
fnobi <shin@fnobi.com>
2012-10-26 20:09:11 +0900

    htmlへの整形に対応

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
-var request   = require('request');
-var cheerio   = {
-       select : require('cheerio-select'),
-       parse  : require('cheerio')
+var request = require('request'),
+    util    = require('util');
+var cheerio = {
+     select : require('cheerio-select'),
+     parse  : require('cheerio')
 };
 
-var linkv = function (url, callback) {
+var linkv = function (url, type, callback) {
 	request.get(url, function (err, res, body) {
 		if (err) {
 			return callback(err);
@@ -12,10 +13,18 @@ var linkv = function (url, callback) {
 
 		var title = getTitle(body);
 
-		var contents = [
-			title,
-			' - ' + url
-		].join('\n');
+		var contents;
+		if (type == 'html') {
+			contents = util.format(
+				'%s - <a href="%s">%s</a>',
+				title, url, url
+			);
+		} else {
+			contents = util.format(
+				'%s\n - %s',
+				title, url
+			);
+		}
 
 		return callback(null, contents);
 	});
@@ -24,7 +33,7 @@ var linkv = function (url, callback) {
 var getTitle = function (html) {
 	var dom = cheerio.parse(html);
 	var filteredDom = cheerio.select('title', dom);
-	return filteredDom[0].children[0].data.replce(/\n/g,'');
+	return filteredDom[0].children[0].data.replace(/\n/g,'');
 };
 
 module.exports = linkv;
